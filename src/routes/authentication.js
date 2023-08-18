6beef253806f4134d0f7225ccfa50106e055273e
stjuliod09 <stivenjd2004@gmail.com>
2023-08-17 23:21:50 -0500

    Ultimos detalles

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ec0f3980a75d621a49cbe74c18d3a48b3cfdcfa180e22414ba3220c30d9f10d
-size 1428
+oid sha256:4ea794d30127cbd71687e7cee430a624212f2f16f58027f197e171b5f6666f9a
+size 1435
